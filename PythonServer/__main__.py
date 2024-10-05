7e77ba6cf49d72eedc401e9962187ab069caeecd
Tom <tom@jaehnig-software-solutions.com>
2024-10-05 20:35:12 +0200

    create python opc ua server

---
+++ PythonServer/__main__.py
@@ -0,0 +1,27 @@
+
+import asyncio
+
+from asyncua import Server, ua
+
+async def main():
+    server = Server()
+    await server.init()
+    server.set_endpoint('opc.tcp://0.0.0.0:4840')
+
+    uri = "http://examples.freeopcua.github.io"
+    idx = await server.register_namespace(uri)
+
+    myobj = await server.nodes.objects.add_object(idx, "MyObject")
+    myvar = await myobj.add_variable(idx, "MyVariable", 6.7)
+    await myvar.set_writable()
+    
+    async with server:
+        while True:
+            await asyncio.sleep(1)
+            new_val = await myvar.get_value() + 0.1
+            print(f"Set value of {myvar} to {new_val}")
+            await myvar.write_value(new_val)
+
+if __name__ == '__main__':
+    print('starting server')
+    asyncio.run(main())
